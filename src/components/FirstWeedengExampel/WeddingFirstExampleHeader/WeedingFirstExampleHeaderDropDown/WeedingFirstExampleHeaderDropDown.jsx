112b39f1b616c4da8cd7daee818912aeb724f9a6
Front-Rent <rparonyan@internet.ru>
2024-08-30 10:10:40 +0400

    changeing email

--- src/components/FirstWeedengExampel/WeddingFirstExampleHeader/WeedingFirstExampleHeaderDropDown/WeedingFirstExampleHeaderDropDown.jsx
+++ src/components/FirstWeedengExampel/WeddingFirstExampleHeader/WeedingFirstExampleHeaderDropDown/WeedingFirstExampleHeaderDropDown.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
-
 import { Link } from "react-scroll";
 
 import "./WeedingFirstExampleHeaderDropDown.scss";
@@ -12,13 +11,15 @@ const HeaderDropDawn = () => {
     setOpen(false);
   }, []);
 
+  const handleCloseMenu = () => {
+    setOpen(false);
+  };
+
   return (
     <div className="weeding-first-example-drop-dawn">
       <AiOutlineMenu
         className="weeding-first-example-drop-dawn-burger-menu"
-        onClick={() => {
-          setOpen(!open);
-        }}
+        onClick={() => setOpen(!open)}
       />
       <div
         className={`weeding-first-example-drop-dawn-menu ${
@@ -27,27 +28,57 @@ const HeaderDropDawn = () => {
       >
         <ul className="weeding-first-example-drop-dawn-menu-ul">
           <li>
-            <Link to="glxavor" smooth={true} duration={500} offset={-70}>
+            <Link
+              to="glxavor"
+              smooth={true}
+              duration={500}
+              offset={-70}
+              onClick={handleCloseMenu}
+            >
               Գլխավոր
             </Link>
           </li>
           <li>
-            <Link to="erbEvVortex" smooth={true} duration={500} offset={-70}>
+            <Link
+              to="erbEvVortex"
+              smooth={true}
+              duration={500}
+              offset={-70}
+              onClick={handleCloseMenu}
+            >
               Երբ և որտեղ
             </Link>
           </li>
           <li>
-            <Link to="hethasvark" smooth={true} duration={500} offset={-70}>
+            <Link
+              to="hethasvark"
+              smooth={true}
+              duration={500}
+              offset={-70}
+              onClick={handleCloseMenu}
+            >
               Հետհաշվարկ
             </Link>
           </li>
           <li>
-            <Link to="manramasner" smooth={true} duration={500} offset={-70}>
+            <Link
+              to="manramasner"
+              smooth={true}
+              duration={500}
+              offset={-70}
+              onClick={handleCloseMenu}
+            >
               Մանրամասներ
             </Link>
           </li>
           <li>
-            <Link to="esHamadzaynEm" smooth={true} duration={500} offset={-70}>
+            <Link
+              to="esHamadzaynEm"
+              smooth={true}
+              duration={500}
+              offset={-70}
+              onClick={handleCloseMenu}
+            >
               Ես Համաձայն եմ
             </Link>
           </li>
